7cfeacc97bbe4d08ed4fbaa39943b9f5100bd3cd
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-22 20:16:58 +0200

    added dynamically shown background depending on the time of day, html and css are finished

--- js/main.js
+++ js/main.js
@@ -28,5 +28,47 @@ function addZero(n){
     return (parseInt(n, 10) < 10 ? '0': '') + n;
 }
 
+// Set Background and Greeting
+function setBgGreet(){
+    let today = new Date(),
+        hour = today.getHours();
+
+    if(hour < 12){
+        // Morning
+        document.body.style.backgroundImage = "url('../img/morning.jpg')";
+        greeting.textContent = 'Good Morning';
+    } else if(hour < 18) {
+        // Afternoon
+        document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon';
+    } else {
+        // Evening
+        document.body.style.backgroundImage = "url('../img/night.jpg')";
+        greeting.textContent = 'Good Evening';
+        document.body.style.color = 'white';
+    }
+}
+
+// Get Name
+function getName(){
+    if(localStorage.getItem('name') === null){
+        name.textContent = 'Enter Name';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// Get Focus
+function getFocus(){
+    if(localStorage.getItem('focus') === null){
+        focus.textContent = 'Enter Focus';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
 // Run
 showTime();
+setBgGreet();
+getName();
+getFocus();
